11d85791621a2855c14d88a664b53f691f75f279
Esteban Vertzner <esteban.vertzner@canda.com>
2024-11-02 18:49:34 +0100

    scaffolding

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        turquoise: {
+          300: '#62E0D9'
+        },
+        mirage: {
+          700: '#202A3C',
+          800: '#181F2B',
+          900: '#1B2330',
+          950: '#0B1524'
+        },
+        stone: {
+          200: '#DFE4F0'
+        },
+        silver: {
+          400: '#C0C0C0'
+        }
+      }
+    }
   },
   plugins: []
 };
