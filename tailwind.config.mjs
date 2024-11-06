3c027897f3d4bac575e672f4100542d2e7bd0943
Esteban Vertzner <esteban.vertzner@canda.com>
2024-11-06 23:33:01 +0100

    mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,13 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      screens: {
+        '2xl': '1440px'
+      },
+      fontFamily: {
+        open: ['Open Sans', 'sans-serif'],
+        raleway: ['Raleway', 'sans-serif']
+      },
       colors: {
         turquoise: {
           300: '#62E0D9'
